853284355510e26c2e10e4f9392fa28f9cd8f47c
CongSanhLe <congsanhle1999@gmail.com>
2023-07-20 05:24:33 +0700

    exe auto open site

--- work_auto/worksetupAuto.py
+++ work_auto/worksetupAuto.py
@@ -2,12 +2,20 @@ import webbrowser as wb
 from var import WEB_URLS
 
 
+# def workAuto():
+#     Edge_path = "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe %s"
+#     url = input("Enter name of the website:# ")
+#     wb.get(Edge_path).open(url)
+
+
+# workAuto()
+
+
 def workAuto():
     Edge_path = "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe %s"
-    # URLs = WEB_URLS
-    url = input("Enter name of the website:# ")
-    # for url in URLs:
-    wb.get(Edge_path).open(url)
+    URLs = WEB_URLS
+    for url in URLs:
+        wb.get(Edge_path).open(url)
 
 
 workAuto()
